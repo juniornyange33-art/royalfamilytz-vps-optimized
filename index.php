b903b80f23e8e7e0ac63bdf8220d5696d3db7656
Junior James Nyange <juniornyange33@gmail.com>
2026-09-08 18:19:35 +0300

    updated index

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+declare(strict_types=1);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-declare(strict_types=1);
+
 $secureSession = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
 if (PHP_VERSION_ID >= 70300) {
     session_set_cookie_params([
